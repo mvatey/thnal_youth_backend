6a3f3f2c774828d9a717744483843229db73193b
Sivnean <mar456992@gmail.com>
2026-07-28 18:31:54 +0700

    Revert "Merge feature/donation into feature/member-page"
    This reverts commit a6bdd0ca3b0cd8eef4a898c01e60b4b8a320b7ab, reversing
    changes made to a28e274c4772800396980c8ef9571c3f19fdf1bf.

--- src/main/java/org/example/tnal_youth_backend/authentication/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/tnal_youth_backend/authentication/exception/GlobalExceptionHandler.java
@@ -17,10 +17,7 @@ import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Component;
-
-@Component("authGlobalExceptionHandler")
-@RestControllerAdvice(basePackages = "org.example.tnal_youth_backend.authentication")
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
